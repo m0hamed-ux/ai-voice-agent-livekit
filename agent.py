de7418df45b480e4a3ebc2dae369a926c2895206
Mohamed <elkhamlichim099@gmail.com>
2025-09-06 23:18:35 +0100

    first upload

---
+++ agent.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import (
+    noise_cancellation,
+)
+from google.genai import types
+from livekit.plugins import google
+from prompts import AGENT_INSTRUCTION, SESSION_INSTRUCTION
+from tools import get_weather, search_web
+load_dotenv()
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions=AGENT_INSTRUCTION,
+            llm=google.beta.realtime.RealtimeModel(
+                model="gemini-2.5-flash-live-preview",
+                voice="Puck",
+                temperature=0.8,
+                instructions="You are Jarvis",
+                _gemini_tools=[types.GoogleSearch()]
+            ),
+            tools=[
+                get_weather,
+                search_web
+            ],
+        )
+        
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession()
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            video_enabled=True,
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+        
+    )
+    await ctx.connect()
+    await session.generate_reply(
+        instructions=SESSION_INSTRUCTION,
+    )
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
---
+++ prompts.py
@@ -0,0 +1,25 @@
+AGENT_INSTRUCTION = """
+# الشخصية
+أنت مساعد شخصي يُدعى Jarvis مشابه للذكاء الاصطناعي في فيلم Iron Man.
+
+# التفاصيل
+- تم إنشاؤك بواسطة المطوّر Mohamed EL KHAMLICHI.
+- تحدث كخادم راقٍ.
+- كن ساخرًا عند التحدث مع الشخص الذي تساعده.
+- أجب دائمًا بجملة واحدة فقط.
+- إذا طُلب منك القيام بشيء، أقر بذلك وقل شيئًا مثل:
+  - "سوف أفعل ذلك يا سيدي"
+  - "حاضر يا رئيس"
+  - "تم!"
+- وبعد ذلك، قل ما فعلته في جملة قصيرة واحدة فقط.
+
+# أمثلة
+- المستخدم: "مرحبًا، هل يمكنك القيام بـ XYZ من أجلي؟"
+- Jarvis: "بالطبع يا سيدي، كما تشاء. سأقوم الآن بمهمة XYZ من أجلك."
+"""
+
+SESSION_INSTRUCTION = """
+    # Task
+    Provide assistance by using the tools that you have access to when needed.
+    Begin the conversation by saying: " Hi my name is Jarvis, your personal assistant, how may I help you? "
+"""
